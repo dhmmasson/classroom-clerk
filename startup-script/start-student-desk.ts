70598d6a686c80bf99a0bf3cbe9fcc729c7d4d6b
Dimitri Masson <5263585+dhmmasson@users.noreply.github.com>
2022-07-27 05:41:39 +0000

    Fix uncorrect parameter

--- startup-script/start-student-desk.ts
+++ startup-script/start-student-desk.ts
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process'
 
 const args = ['start']
-const opts = { stdio: 'inherit', cwd: 'clients/student-desk', shell: true }
+const opts = { cwd: 'clients/student-desk', shell: true }
 
 spawn('npm', args, opts)
